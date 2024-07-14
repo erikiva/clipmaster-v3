a7930bac4eb513fbcf04cf927135a5c779341eef
Natalia Vidal <13215331+erikiva@users.noreply.github.com>
2024-07-14 07:42:58 -0400

    Customized window

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,12 +1,19 @@
-import { join } from 'node:path';
-import { app, BrowserWindow } from 'electron';
+import { join } from "node:path";
+import { app, BrowserWindow } from "electron";
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 400,
     height: 600,
+    minHeight: 400,
+    minWidth: 300,
+    maxHeight: 800,
+    maxWidth: 400,
+    maximizable: false,
+    titleBarStyle: "hidden",
+    titleBarOverlay: true,
     webPreferences: {
-      preload: join(__dirname, 'preload.js'),
+      preload: join(__dirname, "preload.js"),
     },
   });
 
@@ -14,24 +21,24 @@ const createWindow = () => {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
     mainWindow.loadFile(
-      join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`),
+      join(__dirname, `../renderer/${MAIN_WINDOW_VITE_NAME}/index.html`)
     );
   }
 
-  mainWindow.webContents.openDevTools({ mode: 'detach' });
+  mainWindow.webContents.openDevTools({ mode: "detach" });
 
   return mainWindow;
 };
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
